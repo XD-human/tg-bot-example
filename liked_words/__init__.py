e1fafc1e8c2a47e93117d47f289d0106c7faf722
Lucky Swan <a.lebedev080318@gmail.com>
2024-02-21 18:38:30 +0300

    get_words() func, rework for having multiple words (with & as separator)

--- liked_words/__init__.py
+++ liked_words/__init__.py
@@ -1,11 +1,12 @@
 import sqlite3
 from sqlite3 import Connection, Cursor
+from typing import Optional
 
-PATH = "./liked_words.db"
+DEFAULT_PATH = "../liked_words.db"
 
 
-def connect() -> tuple[Connection, Cursor]:
-    con = sqlite3.connect(PATH)
+def connect(path=DEFAULT_PATH) -> tuple[Connection, Cursor]:
+    con = sqlite3.connect(DEFAULT_PATH)
     cur = con.cursor()
     return con, cur
 
@@ -15,9 +16,9 @@ def initialize():
 
     cur.execute("""
         CREATE TABLE IF NOT EXISTS "liked_words"  (
-            "index"	INTEGER UNIQUE,
-            "word"	TEXT,
-            PRIMARY KEY("index" AUTOINCREMENT)
+            "chatId"	INTEGER UNIQUE,
+            "words"	TEXT,
+            PRIMARY KEY("chatId")
         );
     """)
 
@@ -25,15 +26,38 @@ def initialize():
     con.close()
 
 
-def add_word(word: str):
+def add_word(chat_id: int, word: str):
     con, cur = connect()
-    cur.execute("INSERT INTO liked_words (word) VALUES (?)", (word,))
+    current_words = get_words(chat_id)
+    if current_words is None:
+        cur.execute("INSERT INTO liked_words (chatId, words) VALUES (?, ?)", (chat_id, word))
+    else:
+        current_words.append(word)
+        string_repr = "&".join(current_words)
+        cur.execute("UPDATE liked_words SET words=(?) WHERE chatId=(?)", (string_repr, chat_id))
     con.commit()
     con.close()
 
 
 def remove_word(word: str):
     con, cur = connect()
-    cur.execute("DELETE FROM liked_words WHERE word=(?)", (word,))
+    cur.execute("DELETE FROM liked_words WHERE words=(?)", (word,))
     con.commit()
     con.close()
+
+
+def get_words(chat_id: int) -> Optional[list[str]]:
+    con, cur = connect()
+    cur.execute("SELECT words FROM liked_words WHERE chatId=(?)", (chat_id,))
+    data = cur.fetchone()
+
+    if data is None:
+        return None
+
+    words = data[0].split("&")
+    return words
+
+
+if __name__ == "__main__":
+    initialize()
+    add_word(2, "что-то")
